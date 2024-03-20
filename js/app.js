f709617d466d9f46676c6692cee4108c38d2ae2c
mukh4mmadov <omuhammadov467@gmail.com>
2024-03-21 01:21:34 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    title: "tip-calculator",
+    githubLink: "https://github.com/mukh4mmadov/tip-calculator1",
+    vercelLink: "https://tip-calculator1-gamma.vercel.app/",
+  },
+];
+
+const template = document.querySelector("template");
+const worksList = document.querySelector(".works__list");
+
+projects.forEach((project) => {
+  const clone = template.content.cloneNode(true);
+
+  const title = clone.querySelector(".works__list__title");
+  const linkGithub = clone.querySelector(".works__list__link__github");
+  const linkVercel = clone.querySelector(".works__list__link__vercel");
+
+  title.textContent = project.title;
+  linkGithub.href = project.githubLink;
+  linkVercel.href = project.vercelLink;
+
+  worksList.appendChild(clone);
+});
